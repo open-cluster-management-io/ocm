6df209e109f10af1f7d1dde9e61008480d253909
Jian Qiu <jqiu@redhat.com>
2021-04-15 12:59:16 +0800

    Add health check for addon
    Signed-off-by: Jian Qiu <jqiu@redhat.com>

--- pkg/lease/lease_controller_test.go
+++ pkg/lease/lease_controller_test.go
@@ -4,9 +4,11 @@ import (
 	"context"
 	"testing"
 
+	"github.com/open-cluster-management/addon-framework/pkg/addonmanager/addontesting"
 	corev1 "k8s.io/api/core/v1"
+	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
+	"k8s.io/apimachinery/pkg/runtime"
 	kubefake "k8s.io/client-go/kubernetes/fake"
-	"k8s.io/client-go/kubernetes/scheme"
 )
 
 const (
@@ -14,9 +16,7 @@ const (
 	agentNs   = "open-cluster-management-agent"
 )
 
-func TestLeaseReconciler_Reconcile(t *testing.T) {
-	s := scheme.Scheme
-	s.AddKnownTypes(corev1.SchemeGroupVersion, &corev1.Namespace{})
+func TestReconcile(t *testing.T) {
 	kubeClient := kubefake.NewSimpleClientset()
 
 	leaseReconciler := &leaseUpdater{
@@ -28,22 +28,84 @@ func TestLeaseReconciler_Reconcile(t *testing.T) {
 
 	//create lease
 	leaseReconciler.reconcile(context.TODO())
-	if !actionExist(kubeClient, "create") {
-		t.Errorf("failed to create lease")
-	}
+	addontesting.AssertActions(t, kubeClient.Actions(), "get", "create")
 
 	//update lease
+	kubeClient.ClearActions()
 	leaseReconciler.reconcile(context.TODO())
-	if !actionExist(kubeClient, "update") {
-		t.Errorf("failed to update lease")
-	}
+	addontesting.AssertActions(t, kubeClient.Actions(), "get", "update")
 }
 
-func actionExist(kubeClient *kubefake.Clientset, existAction string) bool {
-	for _, action := range kubeClient.Actions() {
-		if action.GetVerb() == existAction {
-			return true
-		}
+func TestReconcileWithHealthCheck(t *testing.T) {
+	kubeClient := kubefake.NewSimpleClientset()
+
+	healthy := false
+	leaseReconciler := &leaseUpdater{
+		kubeClient:           kubeClient,
+		leaseName:            leaseName,
+		leaseDurationSeconds: 1,
+		leaseNamespace:       agentNs,
+		healthCheckFuncs: []func() bool{
+			func() bool { return healthy },
+		},
 	}
-	return false
+
+	//create lease
+	leaseReconciler.reconcile(context.TODO())
+	addontesting.AssertNoActions(t, kubeClient.Actions())
+
+	healthy = true
+	leaseReconciler.reconcile(context.TODO())
+	addontesting.AssertActions(t, kubeClient.Actions(), "get", "create")
+}
+
+func TestCheckAddonPodFunc(t *testing.T) {
+	cases := []struct {
+		name     string
+		pods     []runtime.Object
+		expected bool
+	}{
+		{
+			name:     "no pod",
+			pods:     []runtime.Object{},
+			expected: false,
+		},
+		{
+			name: "incorrect label",
+			pods: []runtime.Object{
+				&corev1.Pod{ObjectMeta: metav1.ObjectMeta{Name: "pod-1", Namespace: "test"}},
+			},
+			expected: false,
+		},
+		{
+			name: "no running state",
+			pods: []runtime.Object{
+				&corev1.Pod{ObjectMeta: metav1.ObjectMeta{Name: "pod-1", Namespace: "test", Labels: map[string]string{"addon": "test"}}},
+				&corev1.Pod{ObjectMeta: metav1.ObjectMeta{Name: "pod-2", Namespace: "test", Labels: map[string]string{"addon": "test"}}},
+			},
+			expected: false,
+		},
+		{
+			name: "running state",
+			pods: []runtime.Object{
+				&corev1.Pod{ObjectMeta: metav1.ObjectMeta{Name: "pod-1", Namespace: "test", Labels: map[string]string{"addon": "test"}}},
+				&corev1.Pod{
+					ObjectMeta: metav1.ObjectMeta{Name: "pod-2", Namespace: "test", Labels: map[string]string{"addon": "test"}},
+					Status:     corev1.PodStatus{Phase: corev1.PodRunning},
+				},
+			},
+			expected: true,
+		},
+	}
+
+	for _, c := range cases {
+		t.Run(c.name, func(t *testing.T) {
+			kubeClient := kubefake.NewSimpleClientset(c.pods...)
+			actual := CheckAddonPodFunc(kubeClient.CoreV1(), "test", "addon=test")()
+			if c.expected != actual {
+				t.Errorf("Failed to check pod expect %v, actual %v", c.expected, actual)
+			}
+		})
+	}
+
 }
