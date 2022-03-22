a45f8bcc4be1ec48aeed52601ef7f4edffc39f0f
Jian Qiu <jqiu@redhat.com>
2022-03-22 14:19:22 +0800

    Add disable-leader-election flag (#221)
    Signed-off-by: Jian Qiu <jqiu@redhat.com>

--- pkg/cmd/operator/spoke.go
+++ pkg/cmd/operator/spoke.go
@@ -13,12 +13,14 @@ import (
 func NewKlusterletOperatorCmd() *cobra.Command {
 
 	options := klusterlet.Options{}
-	cmd := controllercmd.
-		NewControllerCommandConfig("klusterlet", version.Get(), options.RunKlusterletOperator).
-		NewCommand()
+	cmdConfig := controllercmd.
+		NewControllerCommandConfig("klusterlet", version.Get(), options.RunKlusterletOperator)
+	cmd := cmdConfig.NewCommand()
 	cmd.Use = "klusterlet"
 	cmd.Short = "Start the klusterlet operator"
 
+	// add disable leader election flag
+	cmd.Flags().BoolVar(&cmdConfig.DisableLeaderElection, "disable-leader-election", false, "Disable leader election for the agent.")
 	cmd.Flags().BoolVar(&options.SkipPlaceholderHubSecret, "skip-placeholder-hub-secret", false,
 		"If set, will skip ensuring a placeholder hub secret which is originally intended for pulling "+
 			"work image before approved")
