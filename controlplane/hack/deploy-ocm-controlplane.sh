4fa9f5d9dff27555150b94f22e5718e9477aa0b2
Chunlin Yang <chuyang@redhat.com>
2022-11-14 21:44:09 +0800

    pass the validation (#95)
    Signed-off-by: clyang82 <chuyang@redhat.com>
    
    Signed-off-by: clyang82 <chuyang@redhat.com>

--- controlplane/hack/deploy-ocm-controlplane.sh
+++ controlplane/hack/deploy-ocm-controlplane.sh
@@ -146,7 +146,7 @@ function start_apiserver {
     cp ${CERT_DIR}/kube-aggregator.kubeconfig ${CERT_DIR}/kubeconfig
     cp hack/deploy/deployment.yaml hack/deploy/deployment.yaml.tmp
     sed -e 's,API_HOST,'${API_HOST}',' hack/deploy/deployment.yaml
-    ${KUSTOMIZE} build hack/deploy
+    ${KUSTOMIZE} build hack/deploy | ${KUBECTL} apply -f -
     mv hack/deploy/deployment.yaml.tmp hack/deploy/deployment.yaml
 
     echo "Use '${KUBECTL} --kubeconfig=${CERT_DIR}/kubeconfig' to use the aggregated API server" 
