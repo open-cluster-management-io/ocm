4c6270e80d5bc1510d18525f24f2d11e75a2415a
Jian Qiu <jqiu@redhat.com>
2023-09-04 16:14:15 +0800

    Remove old agent when upgrade to singleton
    Signed-off-by: Jian Qiu <jqiu@redhat.com>

--- pkg/work/helper/helpers.go
+++ pkg/work/helper/helpers.go
@@ -483,7 +483,8 @@ func (pdl PlacementDecisionGetter) List(selector labels.Selector, namespace stri
 // Get added and deleted clusters names
 func GetClusters(client clusterlister.PlacementDecisionLister, placement *clusterv1beta1.Placement,
 	existingClusters sets.Set[string]) (sets.Set[string], sets.Set[string], error) {
-	pdtracker := clusterv1beta1.NewPlacementDecisionClustersTracker(placement, PlacementDecisionGetter{Client: client}, existingClusters)
+	pdtracker := clusterv1beta1.NewPlacementDecisionClustersTracker(
+		placement, PlacementDecisionGetter{Client: client}, existingClusters)
 
 	return pdtracker.Get()
 }
