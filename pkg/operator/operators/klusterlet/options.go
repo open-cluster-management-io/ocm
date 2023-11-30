7ceb9a2bce3ada7334be089d411337f97b0435e6
Jian Qiu <jqiu@redhat.com>
2023-11-30 21:05:39 +0800

    Reduce permission for cluster manager and klusterlet (#325)
    Signed-off-by: Jian Qiu <jqiu@redhat.com>

--- pkg/operator/operators/klusterlet/options.go
+++ pkg/operator/operators/klusterlet/options.go
@@ -2,7 +2,7 @@ package klusterlet
 
 import (
 	"context"
-	"io/ioutil"
+	"os"
 	"time"
 
 	"github.com/openshift/library-go/pkg/controller/controllercmd"
@@ -65,7 +65,7 @@ func (o *Options) RunKlusterletOperator(ctx context.Context, controllerContext *
 
 	hubConfigSecretInformer := newOneTermInformer(helpers.HubKubeConfig)
 	bootstrapConfigSecretInformer := newOneTermInformer(helpers.BootstrapHubKubeConfig)
-	externalConfigSecretInformer := newOneTermInformer(helpers.WorkWebhookSecret)
+	externalConfigSecretInformer := newOneTermInformer(helpers.ExternalManagedKubeConfig)
 
 	secretInformers := map[string]corev1informers.SecretInformer{
 		helpers.HubKubeConfig:             hubConfigSecretInformer.Core().V1().Secrets(),
@@ -92,7 +92,7 @@ func (o *Options) RunKlusterletOperator(ctx context.Context, controllerContext *
 
 	// Read component namespace
 	operatorNamespace := defaultComponentNamespace
-	nsBytes, err := ioutil.ReadFile("/var/run/secrets/kubernetes.io/serviceaccount/namespace")
+	nsBytes, err := os.ReadFile("/var/run/secrets/kubernetes.io/serviceaccount/namespace")
 	if err == nil {
 		operatorNamespace = string(nsBytes)
 	}
