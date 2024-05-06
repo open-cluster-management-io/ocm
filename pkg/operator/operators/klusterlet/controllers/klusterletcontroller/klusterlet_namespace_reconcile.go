5fc1dbdce72a3ad192fbb48495e303ab210429d9
Jian Qiu <jqiu@redhat.com>
2024-05-06 11:15:48 +0800

    🐛  Remove old ns when klusterlet ns is changed (#442)
    * Remove old ns when klusterlet ns is changed
    
    Signed-off-by: Jian Qiu <jqiu@redhat.com>
    
    * Resolve comments
    
    Signed-off-by: Jian Qiu <jqiu@redhat.com>
    
    ---------
    
    Signed-off-by: Jian Qiu <jqiu@redhat.com>

---
+++ pkg/operator/operators/klusterlet/controllers/klusterletcontroller/klusterlet_namespace_reconcile.go
@@ -0,0 +1,55 @@
+package klusterletcontroller
+
+import (
+	"context"
+	"fmt"
+
+	"k8s.io/apimachinery/pkg/api/errors"
+	"k8s.io/apimachinery/pkg/api/meta"
+	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
+
+	operatorapiv1 "open-cluster-management.io/api/operator/v1"
+)
+
+// namespaceReconcile is the reconciler to handle the case when spec.namespace is changed, and we need to
+// clean the old namespace. The reconciler should be run as the last reconciler, and only operate when
+// apply status is true, which ensures that old namespace should only be deleted after the agent in new
+// namespace has been deployed.
+type namespaceReconcile struct {
+	managedClusterClients *managedClusterClients
+}
+
+func (r *namespaceReconcile) reconcile(
+	ctx context.Context,
+	klusterlet *operatorapiv1.Klusterlet,
+	config klusterletConfig) (*operatorapiv1.Klusterlet, reconcileState, error) {
+	cond := meta.FindStatusCondition(klusterlet.Status.Conditions, operatorapiv1.ConditionKlusterletApplied)
+	if cond == nil || cond.Status == metav1.ConditionFalse || klusterlet.Generation != klusterlet.Status.ObservedGeneration {
+		return klusterlet, reconcileContinue, nil
+	}
+	// filters namespace for klusterlet and not in hosted mode
+	namespaces, err := r.managedClusterClients.kubeClient.CoreV1().Namespaces().List(ctx, metav1.ListOptions{
+		LabelSelector: fmt.Sprintf(
+			"%s=%s",
+			klusterletNamespaceLabelKey, klusterlet.Name),
+	})
+	if err != nil {
+		return klusterlet, reconcileStop, err
+	}
+	for _, ns := range namespaces.Items {
+		if ns.Name == config.KlusterletNamespace || ns.Name == config.KlusterletNamespace+"-addon" {
+			continue
+		}
+		if err := r.managedClusterClients.kubeClient.CoreV1().Namespaces().Delete(
+			ctx, ns.Name, metav1.DeleteOptions{}); err != nil && !errors.IsNotFound(err) {
+			return klusterlet, reconcileStop, err
+		}
+	}
+	return klusterlet, reconcileContinue, nil
+}
+
+func (r *namespaceReconcile) clean(_ context.Context, klusterlet *operatorapiv1.Klusterlet,
+	_ klusterletConfig) (*operatorapiv1.Klusterlet, reconcileState, error) {
+	// noop
+	return klusterlet, reconcileContinue, nil
+}
