5313ff714ea795f2e96e4b6f381962ce2c318c24
Jian Zhu <jiazhu@redhat.com>
2024-05-14 15:11:37 +0800

    Refresh external managed token secret if service account ns changes (#458)
    Signed-off-by: zhujian <jiazhu@redhat.com>

--- pkg/operator/helpers/sa_syncer_test.go
+++ pkg/operator/helpers/sa_syncer_test.go
@@ -71,7 +71,7 @@ func TestTokenGetter(t *testing.T) {
 				return true, &authv1.TokenRequest{Status: authv1.TokenRequestStatus{Token: "aaa", ExpirationTimestamp: tt.expirationTime}}, nil
 			})
 			tokenGetter := SATokenGetter(context.TODO(), saName, saNamespace, client)
-			token, _, err := tokenGetter()
+			token, _, additionalData, err := tokenGetter()
 			fmt.Printf("client action is %v\n", client.Actions())
 			if err != nil && !tt.wantErr {
 				t.Error(err)
@@ -82,6 +82,18 @@ func TestTokenGetter(t *testing.T) {
 			if !reflect.DeepEqual(token, tt.wantToken) {
 				t.Errorf("token is not correct, got %s", string(token))
 			}
+			if !tt.wantErr {
+				if additionalData == nil {
+					t.Errorf("additional data is nil")
+				}
+				if !reflect.DeepEqual(additionalData["serviceaccount_namespace"], []byte(saNamespace)) {
+					t.Errorf("serviceaccount_namespace is not correct, got %s", string(additionalData["serviceaccount_namespace"]))
+				}
+				if !reflect.DeepEqual(additionalData["serviceaccount_name"], []byte(saName)) {
+					t.Errorf("serviceaccount_name is not correct, got %s", string(additionalData["serviceaccount_name"]))
+				}
+			}
+
 		})
 	}
 }
@@ -100,11 +112,12 @@ func TestApplyKubeconfigSecret(t *testing.T) {
 	now, _ := metav1.Now().MarshalText()
 
 	tests := []struct {
-		name          string
-		secrets       []runtime.Object
-		token         []byte
-		tokenGetError error
-		expiration    *metav1.Time
+		name           string
+		secrets        []runtime.Object
+		token          []byte
+		additionalData map[string][]byte
+		tokenGetError  error
+		expiration     *metav1.Time
 
 		validateActions func(t *testing.T, actions []clienttesting.Action)
 		wantErr         bool
@@ -182,16 +195,76 @@ func TestApplyKubeconfigSecret(t *testing.T) {
 				}
 			},
 		},
+		{
+			name:  "update secret if additional data changed",
+			token: []byte("aaa"),
+			secrets: []runtime.Object{
+				&corev1.Secret{
+					ObjectMeta: metav1.ObjectMeta{
+						Name:      secretName,
+						Namespace: secretNamespace,
+					},
+					Data: map[string][]byte{
+						"token":                    []byte("aaa"),
+						"serviceaccount_name":      []byte("test-sa"),
+						"serviceaccount_namespace": []byte("test-ns"),
+					},
+				},
+			},
+			additionalData: map[string][]byte{
+				"serviceaccount_name":      []byte("test-sa"),
+				"serviceaccount_namespace": []byte("test-ns-new"),
+			},
+			validateActions: func(t *testing.T, actions []clienttesting.Action) {
+				if len(actions) != 4 {
+					t.Fatalf("expect 4 actions, but get %d", len(actions))
+				}
+
+				secret := actions[3].(clienttesting.CreateAction).GetObject().(*corev1.Secret)
+				if !reflect.DeepEqual(secret.Data["token"], []byte("aaa")) {
+					t.Errorf("secret update is not correct, got %v", secret.Data)
+				}
+			},
+		},
+		{
+			name:  "update secret if new additional data added",
+			token: []byte("aaa"),
+			secrets: []runtime.Object{
+				&corev1.Secret{
+					ObjectMeta: metav1.ObjectMeta{
+						Name:      secretName,
+						Namespace: secretNamespace,
+					},
+					Data: map[string][]byte{
+						"token": []byte("aaa"),
+					},
+				},
+			},
+			additionalData: map[string][]byte{
+				"serviceaccount_name":      []byte("test-sa"),
+				"serviceaccount_namespace": []byte("test-ns"),
+			},
+			validateActions: func(t *testing.T, actions []clienttesting.Action) {
+				if len(actions) != 4 {
+					t.Fatalf("expect 4 actions, but get %d", len(actions))
+				}
+
+				secret := actions[3].(clienttesting.CreateAction).GetObject().(*corev1.Secret)
+				if !reflect.DeepEqual(secret.Data["token"], []byte("aaa")) {
+					t.Errorf("secret update is not correct, got %v", secret.Data)
+				}
+			},
+		},
 	}
 	for _, tt := range tests {
 		t.Run(tt.name, func(t *testing.T) {
-			tokenGetter := func() ([]byte, []byte, error) {
+			tokenGetter := func() ([]byte, []byte, map[string][]byte, error) {
 				if tt.expiration == nil {
-					return tt.token, nil, tt.tokenGetError
+					return tt.token, nil, tt.additionalData, tt.tokenGetError
 				}
 
 				expiration, _ := tt.expiration.MarshalText()
-				return tt.token, expiration, tt.tokenGetError
+				return tt.token, expiration, tt.additionalData, tt.tokenGetError
 			}
 			client := testclient.NewSimpleClientset(tt.secrets...)
 			err := SyncKubeConfigSecret(
