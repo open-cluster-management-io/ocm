d5421159c21620b651f2d323a91a786ef8fe19b8
Rokibul Hasan <mdrokibulhasan@appscode.com>
2024-06-02 10:24:32 +0600

    Run make fmt-imports
    Signed-off-by: Rokibul Hasan <mdrokibulhasan@appscode.com>

--- pkg/work/webhook/start.go
+++ pkg/work/webhook/start.go
@@ -3,13 +3,14 @@ package webhook
 import (
 	"context"
 	"crypto/tls"
+
 	"k8s.io/apimachinery/pkg/runtime"
-	"k8s.io/klog/v2"
 	// Import all Kubernetes client auth plugins (e.g. Azure, GCP, OIDC, etc.)
 	// to ensure that exec-entrypoint and run can make use of them.
 	utilruntime "k8s.io/apimachinery/pkg/util/runtime"
 	clientgoscheme "k8s.io/client-go/kubernetes/scheme"
 	_ "k8s.io/client-go/plugin/pkg/client/auth"
+	"k8s.io/klog/v2"
 	ctrl "sigs.k8s.io/controller-runtime"
 	"sigs.k8s.io/controller-runtime/pkg/healthz"
 	"sigs.k8s.io/controller-runtime/pkg/webhook"
