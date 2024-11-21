be3d02450ab3d7dce1f6d5d77cfddc6f206bb33f
Wei Liu <liuweixa@redhat.com>
2024-11-21 17:48:10 +0800

    test-7
    Signed-off-by: Wei Liu <liuweixa@redhat.com>

--- pkg/work/spoke/spokeagent.go
+++ pkg/work/spoke/spokeagent.go
@@ -43,7 +43,7 @@ const (
 	appliedManifestWorkFinalizeControllerWorkers = 10
 	manifestWorkFinalizeControllerWorkers        = 10
 	availableStatusControllerWorkers             = 10
-	test                                         = 6
+	test                                         = 7
 )
 
 type WorkAgentConfig struct {
