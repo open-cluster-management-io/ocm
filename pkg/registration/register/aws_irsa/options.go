0acf030cdb6bfee6ca0f7ddb56cd34cbfb3851b8
jaswalkiranavtar <jaswalkiranavtar@gmail.com>
2025-01-09 14:07:43 -0500

    :sparkles: Completing aws registration on spoke (#788)
    * Completing aws registration on spoke
    
    Signed-off-by: suvaanshkumar <suvaanshkumar@gmail.com>
    
    * adding new function to overcome gci errors on slices
    
    Signed-off-by: suvaanshkumar <suvaanshkumar@gmail.com>
    
    * Refactoring array contains function
    
    Signed-off-by: Gaurav Jaswal <jaswalkiranavtar@gmail.com>
    
    ---------
    
    Signed-off-by: suvaanshkumar <suvaanshkumar@gmail.com>
    Signed-off-by: Gaurav Jaswal <jaswalkiranavtar@gmail.com>
    Co-authored-by: suvaanshkumar <suvaanshkumar@gmail.com>

--- pkg/registration/register/aws_irsa/options.go
+++ pkg/registration/register/aws_irsa/options.go
@@ -2,7 +2,6 @@ package aws_irsa
 
 import (
 	"fmt"
-	"strings"
 
 	"github.com/openshift/library-go/pkg/controller/factory"
 	"k8s.io/apimachinery/pkg/api/meta"
@@ -10,7 +9,6 @@ import (
 	addonv1alpha1 "open-cluster-management.io/api/addon/v1alpha1"
 	hubclusterclientset "open-cluster-management.io/api/client/cluster/clientset/versioned"
 	managedclusterinformers "open-cluster-management.io/api/client/cluster/informers/externalversions/cluster"
-	clusterv1 "open-cluster-management.io/api/cluster/v1"
 
 	"open-cluster-management.io/ocm/pkg/registration/register"
 )
@@ -18,8 +16,7 @@ import (
 // AWSOption includes options that is used to monitor ManagedClusters
 type AWSOption struct {
 	EventFilterFunc factory.EventFilterFunc
-
-	AWSIRSAControl AWSIRSAControl
+	AWSIRSAControl  AWSIRSAControl
 }
 
 func NewAWSOption(
@@ -35,16 +32,11 @@ func NewAWSOption(
 	}
 	return &AWSOption{
 		EventFilterFunc: func(obj interface{}) bool {
-			// TODO: implement EventFilterFunc and update below
 			accessor, err := meta.Accessor(obj)
 			if err != nil {
 				return false
 			}
 			labels := accessor.GetLabels()
-			// only enqueue csr from a specific managed cluster
-			if labels[clusterv1.ClusterNameLabelKey] != secretOption.ClusterName {
-				return false
-			}
 
 			// should not contain addon key
 			_, ok := labels[addonv1alpha1.AddonLabelKey]
@@ -53,7 +45,7 @@ func NewAWSOption(
 			}
 
 			// only enqueue csr whose name starts with the cluster name
-			return strings.HasPrefix(accessor.GetName(), fmt.Sprintf("%s-", secretOption.ClusterName))
+			return accessor.GetName() == secretOption.ClusterName
 		},
 		AWSIRSAControl: awsIrsaControl,
 	}, nil
