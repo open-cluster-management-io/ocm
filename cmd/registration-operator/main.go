88067f5b7e720208a5b5ce633d01d0d189936412
xuezhaojun <zxue@redhat.com>
2025-02-01 10:45:44 +0800

    Remove unnecessary random seed initialization.
    Signed-off-by: xuezhaojun <zxue@redhat.com>

--- cmd/registration-operator/main.go
+++ cmd/registration-operator/main.go
@@ -3,9 +3,7 @@ package main
 import (
 	goflag "flag"
 	"fmt"
-	"math/rand"
 	"os"
-	"time"
 
 	"github.com/spf13/cobra"
 	"github.com/spf13/pflag"
@@ -18,8 +16,6 @@ import (
 )
 
 func main() {
-	rand.Seed(time.Now().UTC().UnixNano())
-
 	pflag.CommandLine.SetNormalizeFunc(utilflag.WordSepNormalizeFunc)
 	pflag.CommandLine.AddGoFlagSet(goflag.CommandLine)
 
