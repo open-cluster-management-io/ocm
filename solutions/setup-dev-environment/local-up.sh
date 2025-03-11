e4eff8b38440629cc403f3e56ce0bc55bfd6053c
Anurag Nayak <nayakanurag078@gmail.com>
2025-03-11 02:37:31 -0700

    docs: Made some typo fixes in the setup script (#883)
    Signed-off-by: Anu-Ra-g <nayakanurag078@gmail.com>

--- solutions/setup-dev-environment/local-up.sh
+++ solutions/setup-dev-environment/local-up.sh
@@ -16,17 +16,17 @@ kind create cluster --name "${hub}"
 kind create cluster --name "${c1}"
 kind create cluster --name "${c2}"
 
-echo "Initialize the ocm hub cluster\n"
+echo -e "Initialize the ocm hub cluster\n"
 clusteradm init --wait --context ${hubctx}
 joincmd=$(clusteradm get token --context ${hubctx} | grep clusteradm)
 
-echo "Join cluster1 to hub\n"
+echo -e "Join cluster1 to hub\n"
 $(echo ${joincmd} --force-internal-endpoint-lookup --wait --context ${c1ctx} | sed "s/<cluster_name>/$c1/g")
 
-echo "Join cluster2 to hub\n"
+echo -e "Join cluster2 to hub\n"
 $(echo ${joincmd} --force-internal-endpoint-lookup --wait --context ${c2ctx} | sed "s/<cluster_name>/$c2/g")
 
-echo "Accept join of cluster1 and cluster2"
+echo -e "Accept join of cluster1 and cluster2\n"
 clusteradm accept --context ${hubctx} --clusters ${c1},${c2} --wait
 
 kubectl get managedclusters --all-namespaces --context ${hubctx}
