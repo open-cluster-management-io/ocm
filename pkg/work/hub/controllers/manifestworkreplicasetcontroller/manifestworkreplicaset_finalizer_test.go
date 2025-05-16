c535d3338be069b10a6aea62507b1e4e50a6a010
Zhiwei Yin <zyin@redhat.com>
2025-05-16 17:04:53 +0800

    add deletionPolicy for manifestworkReplicaset
    Signed-off-by: Zhiwei Yin <zyin@redhat.com>

--- pkg/work/hub/controllers/manifestworkreplicasetcontroller/manifestworkreplicaset_finalizer_test.go
+++ pkg/work/hub/controllers/manifestworkreplicasetcontroller/manifestworkreplicaset_finalizer_test.go
@@ -6,9 +6,12 @@ import (
 	"time"
 
 	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
+	"k8s.io/apimachinery/pkg/runtime"
 
 	fakeclient "open-cluster-management.io/api/client/work/clientset/versioned/fake"
 	workinformers "open-cluster-management.io/api/client/work/informers/externalversions"
+	workapiv1 "open-cluster-management.io/api/work/v1"
+	workapiv1alpha1 "open-cluster-management.io/api/work/v1alpha1"
 	workapplier "open-cluster-management.io/sdk-go/pkg/apis/work/v1/applier"
 
 	commonhelper "open-cluster-management.io/ocm/pkg/common/helpers"
@@ -17,35 +20,95 @@ import (
 
 // Test finalize reconcile
 func TestFinalizeReconcile(t *testing.T) {
-	mwrSetTest := helpertest.CreateTestManifestWorkReplicaSet("mwrSet-test", "default", "place-test")
-	mw, _ := CreateManifestWork(mwrSetTest, "cluster1", "place-test")
-	fakeClient := fakeclient.NewSimpleClientset(mwrSetTest, mw)
-	manifestWorkInformerFactory := workinformers.NewSharedInformerFactoryWithOptions(fakeClient, 1*time.Second)
-	mwLister := manifestWorkInformerFactory.Work().V1().ManifestWorks().Lister()
+	cases := []struct {
+		name            string
+		resources       func() (*workapiv1alpha1.ManifestWorkReplicaSet, []*workapiv1.ManifestWork)
+		validateActions func(t *testing.T, manifestWorkReplicaSet *workapiv1alpha1.ManifestWorkReplicaSet,
+			state reconcileState, err error)
+	}{
+		{
+			name: "deleting manifestWorkReplicaSet with no pending works",
+			resources: func() (*workapiv1alpha1.ManifestWorkReplicaSet, []*workapiv1.ManifestWork) {
+				mwrSetTest := helpertest.CreateTestManifestWorkReplicaSet("mwrSet-test", "default", "place-test")
+				timeNow := metav1.Now()
+				mwrSetTest.DeletionTimestamp = &timeNow
+				mwrSetTest.Finalizers = append(mwrSetTest.Finalizers, ManifestWorkReplicaSetFinalizer)
 
-	finalizerController := finalizeReconciler{
-		workClient:         fakeClient,
-		manifestWorkLister: mwLister,
-		workApplier:        workapplier.NewWorkApplierWithTypedClient(fakeClient, mwLister),
+				mw, _ := CreateManifestWork(mwrSetTest, "cluster1", "place-test")
+				return mwrSetTest, []*workapiv1.ManifestWork{mw}
+			},
+			validateActions: func(t *testing.T, manifestWorkReplicaSet *workapiv1alpha1.ManifestWorkReplicaSet,
+				state reconcileState, err error) {
+				if err != nil {
+					t.Fatalf("Failed to create manifestWorkReplicaSet: %v", err)
+				}
+				if state != reconcileStop {
+					t.Fatalf("manifestWorkReplicaSet should be stopped after finalizeReconcile")
+				}
+				if commonhelper.HasFinalizer(manifestWorkReplicaSet.Finalizers, ManifestWorkReplicaSetFinalizer) {
+					t.Fatalf("Finalizer %v not deleted", manifestWorkReplicaSet.Finalizers)
+				}
+			},
+		},
+		{
+			name: "deleting Foreground manifestWorkReplicaSet with pending works",
+			resources: func() (*workapiv1alpha1.ManifestWorkReplicaSet, []*workapiv1.ManifestWork) {
+				mwrSetTest := helpertest.CreateTestManifestWorkReplicaSet("mwrSet-test", "default", "place-test")
+				timeNow := metav1.Now()
+				mwrSetTest.DeletionTimestamp = &timeNow
+				mwrSetTest.Finalizers = append(mwrSetTest.Finalizers, ManifestWorkReplicaSetFinalizer)
+				mwrSetTest.Spec.CascadeDeletionPolicy = workapiv1alpha1.Foreground
+
+				mw1, _ := CreateManifestWork(mwrSetTest, "cluster1", "place-test")
+				mw2, _ := CreateManifestWork(mwrSetTest, "cluster2", "place-test")
+				mw2.Finalizers = append(mw2.Finalizers, workapiv1.ManifestWorkFinalizer)
+				return mwrSetTest, []*workapiv1.ManifestWork{mw1, mw2}
+			},
+			validateActions: func(t *testing.T, manifestWorkReplicaSet *workapiv1alpha1.ManifestWorkReplicaSet,
+				state reconcileState, err error) {
+				if err != nil {
+					t.Fatalf("Failed to create manifestWorkReplicaSet: %v", err)
+				}
+				if state != reconcileContinue {
+					t.Fatalf("manifestWorkReplicaSet should be continue after finalizeReconcile")
+				}
+				if !commonhelper.HasFinalizer(manifestWorkReplicaSet.Finalizers, ManifestWorkReplicaSetFinalizer) {
+					t.Fatalf("Finalizer %v is deleted", manifestWorkReplicaSet.Finalizers)
+				}
+			},
+		},
 	}
 
-	// Set manifestWorkReplicaSet delete time AND Set finalizer
-	timeNow := metav1.Now()
-	mwrSetTest.DeletionTimestamp = &timeNow
-	mwrSetTest.Finalizers = append(mwrSetTest.Finalizers, ManifestWorkReplicaSetFinalizer)
+	for _, c := range cases {
+		t.Run(c.name, func(t *testing.T) {
+			manifestWorkReplicaSet, manifestWorks := c.resources()
+			objects := []runtime.Object{manifestWorkReplicaSet}
+			for _, mw := range manifestWorks {
+				objects = append(objects, mw)
+			}
+			fakeClient := fakeclient.NewSimpleClientset(objects...)
+			manifestWorkInformerFactory := workinformers.NewSharedInformerFactoryWithOptions(fakeClient, 1*time.Second)
+			mwLister := manifestWorkInformerFactory.Work().V1().ManifestWorks().Lister()
+			workStore := manifestWorkInformerFactory.Work().V1().ManifestWorks().Informer().GetStore()
+			for _, work := range manifestWorks {
+				if err := workStore.Add(work); err != nil {
+					t.Fatal(err)
+				}
+			}
+			finalizerController := finalizeReconciler{
+				workClient:         fakeClient,
+				manifestWorkLister: mwLister,
+				workApplier:        workapplier.NewWorkApplierWithTypedClient(fakeClient, mwLister),
+			}
 
-	_, _, err := finalizerController.reconcile(context.TODO(), mwrSetTest)
-	if err != nil {
-		t.Fatal(err)
-	}
+			_, state, rstErr := finalizerController.reconcile(context.TODO(), manifestWorkReplicaSet)
 
-	updatetSet, err := fakeClient.WorkV1alpha1().ManifestWorkReplicaSets(mwrSetTest.Namespace).Get(context.TODO(), mwrSetTest.Name, metav1.GetOptions{})
-	if err != nil {
-		t.Fatal(err)
-	}
-
-	// Check mwrSetTest finalizer removed
-	if commonhelper.HasFinalizer(updatetSet.Finalizers, ManifestWorkReplicaSetFinalizer) {
-		t.Fatal("Finalizer not deleted", mwrSetTest.Finalizers)
+			updatetSet, err := fakeClient.WorkV1alpha1().ManifestWorkReplicaSets(manifestWorkReplicaSet.Namespace).
+				Get(context.TODO(), manifestWorkReplicaSet.Name, metav1.GetOptions{})
+			if err != nil {
+				t.Fatalf("Failed to get manifestWorkReplicaSet: %v", err)
+			}
+			c.validateActions(t, updatetSet, state, rstErr)
+		})
 	}
 }
