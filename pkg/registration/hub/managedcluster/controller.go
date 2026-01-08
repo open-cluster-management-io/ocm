ad89f05351e9818d5447c8b362ebcc4090c6f1b0
Érico GR <ericogr@gmail.com>
2026-01-08 10:46:13 -0300

    🐛 Fix work rolebinding cleanup when hubAcceptsClient is set to false (#1318)
    * Fix work rolebinding cleanup when hubAcceptsClient is set to false
    Signed-off-by: Erico G. Rimoli <erico.rimoli@totvs.com.br>
    
    * Adds error handling to the removeClusterRbac call within the controller synchronization function
    Signed-off-by: Erico G. Rimoli <erico.rimoli@totvs.com.br>

--- pkg/registration/hub/managedcluster/controller.go
+++ pkg/registration/hub/managedcluster/controller.go
@@ -181,6 +181,17 @@ func (c *managedClusterController) sync(ctx context.Context, syncCtx factory.Syn
 			}
 		}
 
+		// Remove the cluster role binding files for registration-agent and work-agent.
+		err := c.removeClusterRbac(ctx, syncCtx, managedClusterName, managedCluster.Spec.HubAcceptsClient)
+		if errors.Is(err, requeueError) {
+			syncCtx.Queue().AddAfter(managedClusterName, requeueError.RequeueTime)
+			return nil
+		}
+
+		if err != nil {
+			errs = append(errs, err)
+		}
+
 		if err = c.hubDriver.Cleanup(ctx, managedCluster); err != nil {
 			errs = append(errs, err)
 		}
@@ -200,13 +211,7 @@ func (c *managedClusterController) sync(ctx context.Context, syncCtx factory.Syn
 			return err
 		}
 
-		// Remove the cluster role binding files for registration-agent and work-agent.
-		err = c.removeClusterRbac(ctx, syncCtx, managedClusterName, managedCluster.Spec.HubAcceptsClient)
-		if errors.Is(err, requeueError) {
-			syncCtx.Queue().AddAfter(managedClusterName, requeueError.RequeueTime)
-			return nil
-		}
-		return err
+		return nil
 	}
 
 	// TODO consider to add the managedcluster-namespace.yaml back to staticFiles,
