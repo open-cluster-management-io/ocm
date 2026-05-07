226397e62db6d5fb663eb35ed606d8032298fbed
zhujian <jiazhu@redhat.com>
2026-05-07 18:56:13 +0800

    Add concurrency and index-fix tests for executor cache
    - Add concurrent remove/get, getCacheItems, and cleanup tests to verify
      race-free behavior with -race detector
    - Add TestCacheControllerClusterRoleWithRoleBindingOnly to verify
      clusterRoleEnqueueFu uses byClusterRole index for RoleBindings
    
    Signed-off-by: zhujian <jiazhu@redhat.com>

--- pkg/work/spoke/auth/store/cache_store_test.go
+++ pkg/work/spoke/auth/store/cache_store_test.go
@@ -72,7 +72,7 @@ func TestBasic(t *testing.T) {
 	executor := fmt.Sprintf("%v", 0)
 	for j := 0; j < 10; j++ {
 		d := Dimension{Name: fmt.Sprintf("%v", j)}
-		caches.RemoveByHash(executor, d.Hash())
+		caches.removeByHash(executor, d.Hash())
 	}
 
 	exist = caches.DimensionCachesExists(executor)
@@ -94,4 +94,107 @@ func TestBasic(t *testing.T) {
 	if dimensionNameAccumulate != 45 {
 		t.Errorf("Expected dimension name joining result 45 but got %v", dimensionNameAccumulate)
 	}
+}
+
+func TestConcurrentRemoveAndGet(t *testing.T) {
+	caches := NewExecutorCache()
+	executor := "test-executor"
+
+	for i := 0; i < 100; i++ {
+		d := Dimension{Name: fmt.Sprintf("%d", i)}
+		caches.Upsert(executor, d, nil)
+	}
+
+	wg := sync.WaitGroup{}
+	wg.Add(200)
+	for i := 0; i < 100; i++ {
+		go func(i int) {
+			defer wg.Done()
+			d := Dimension{Name: fmt.Sprintf("%d", i)}
+			caches.removeByHash(executor, d.Hash())
+		}(i)
+		go func(i int) {
+			defer wg.Done()
+			d := Dimension{Name: fmt.Sprintf("%d", i)}
+			caches.Get(executor, d)
+		}(i)
+	}
+	wg.Wait()
+
+	if caches.Count() != 0 {
+		t.Errorf("Expected all items removed but got %d", caches.Count())
+	}
+}
+
+func TestConcurrentGetCacheItemsWhileModifying(t *testing.T) {
+	caches := NewExecutorCache()
+
+	for i := 0; i < 10; i++ {
+		for j := 0; j < 10; j++ {
+			caches.Upsert(fmt.Sprintf("%d", i), Dimension{Name: fmt.Sprintf("%d", j)}, nil)
+		}
+	}
+
+	wg := sync.WaitGroup{}
+	wg.Add(30)
+
+	for i := 0; i < 10; i++ {
+		go func() {
+			defer wg.Done()
+			caches.Count()
+		}()
+	}
+
+	for i := 0; i < 10; i++ {
+		go func(i int) {
+			defer wg.Done()
+			caches.Upsert(fmt.Sprintf("new-%d", i), Dimension{Name: "test"}, nil)
+		}(i)
+	}
+
+	for i := 0; i < 10; i++ {
+		go func(i int) {
+			defer wg.Done()
+			d := Dimension{Name: fmt.Sprintf("%d", 0)}
+			caches.removeByHash(fmt.Sprintf("%d", i), d.Hash())
+		}(i)
+	}
+
+	wg.Wait()
+}
+
+func TestConcurrentCleanupUnnecessaryCaches(t *testing.T) {
+	caches := NewExecutorCache()
+
+	for i := 0; i < 20; i++ {
+		for j := 0; j < 10; j++ {
+			caches.Upsert(fmt.Sprintf("%d", i), Dimension{Name: fmt.Sprintf("%d", j)}, nil)
+		}
+	}
+
+	necessaryCaches := NewExecutorCache()
+	for i := 0; i < 10; i++ {
+		for j := 0; j < 5; j++ {
+			necessaryCaches.Upsert(fmt.Sprintf("%d", i), Dimension{Name: fmt.Sprintf("%d", j)}, nil)
+		}
+	}
+
+	wg := sync.WaitGroup{}
+	wg.Add(11)
+
+	go func() {
+		defer wg.Done()
+		caches.CleanupUnnecessaryCaches(necessaryCaches)
+	}()
+
+	for i := 0; i < 10; i++ {
+		go func(i int) {
+			defer wg.Done()
+			executor := fmt.Sprintf("%d", i)
+			d := Dimension{Name: fmt.Sprintf("%d", i)}
+			caches.Get(executor, d)
+		}(i)
+	}
+
+	wg.Wait()
 }
