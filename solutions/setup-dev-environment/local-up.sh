14a8ce72876c07dcfabeeed84b745630827e7698
kahirokunn <okinakahiro@gmail.com>
2026-05-08 15:47:05 +0900

    Register hub as local-cluster in local-up
    Signed-off-by: kahirokunn <okinakahiro@gmail.com>

--- solutions/setup-dev-environment/local-up.sh
+++ solutions/setup-dev-environment/local-up.sh
@@ -4,9 +4,10 @@ cd $(dirname ${BASH_SOURCE})
 
 set -e
 
-hub=${CLUSTER1:-hub}
+hub=${HUB:-hub}
 c1=${CLUSTER1:-cluster1}
 c2=${CLUSTER2:-cluster2}
+local_cluster=local-cluster
 
 hubctx="kind-${hub}"
 c1ctx="kind-${c1}"
@@ -26,7 +27,13 @@ $(echo ${joincmd} --force-internal-endpoint-lookup --wait --context ${c1ctx} | s
 echo -e "Join cluster2 to hub\n"
 $(echo ${joincmd} --force-internal-endpoint-lookup --wait --context ${c2ctx} | sed "s/<cluster_name>/$c2/g")
 
-echo -e "Accept join of cluster1 and cluster2\n"
-clusteradm accept --context ${hubctx} --clusters ${c1},${c2} --wait
+echo -e "Join hub to itself as ${local_cluster}\n"
+$(echo ${joincmd} --force-internal-endpoint-lookup --wait --context ${hubctx} | sed "s/<cluster_name>/${local_cluster}/g")
+
+managed_clusters="${c1},${c2},${local_cluster}"
+echo -e "Accept join of ${managed_clusters}\n"
+clusteradm accept --context ${hubctx} --clusters ${managed_clusters} --wait
+
+kubectl label managedcluster "${local_cluster}" local-cluster=true --overwrite --context ${hubctx}
 
 kubectl get managedclusters --all-namespaces --context ${hubctx}
